b9cc1968502c2b9a57d56cf39787a8accfffae97
stikie123 <stianbekker@gmail.com>
2023-05-30 20:23:21 +0200

    Added features 2 and 3, bugs fixed, more scalable

--- Program.cs
+++ Program.cs
@@ -11,9 +11,7 @@ string? readResult;
 string menuSelection = "";
 
 
-XmlDocument doc = new XmlDocument();
-doc.Load(@"data.xml");
-XmlNodeList nodes = doc.DocumentElement.SelectNodes("/workouts/Workout");
+
 
 List<XmlNode> nodeList = new List<XmlNode>();
 
@@ -39,7 +37,11 @@ do
     switch (menuSelection)
     {
         case "1":
-            foreach (XmlNode node in nodes)
+            XmlDocument calDoc = new XmlDocument();
+            calDoc.Load(@"calData.xml");
+            XmlNodeList calNodes = calDoc.DocumentElement.SelectNodes("/workouts/Workout");
+
+            foreach (XmlNode node in calNodes)
             {
                 if (node.Attributes["category"].Value == "calisthenics")
                 {
@@ -61,28 +63,85 @@ do
             {
                 Console.WriteLine("Name: " + node["name"].InnerText + " Sets: " + node["sets"].InnerText + " Rest: " + node["rest"].InnerText);
             }
-
+            nodeList = new List<XmlNode>();
             Console.WriteLine("\n\rPress the Enter key to continue");
             readResult = Console.ReadLine();
 
             break;
 
         case "2":
-            string? readResult2;
-            string menuSelection2 = "";
-            Console.WriteLine("What would you like to focus on");
-            Console.WriteLine("1. Full body");
-            Console.WriteLine("2. Upper body");
-            Console.WriteLine("3. Lower body");
-            Console.WriteLine();
-            Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
+            XmlDocument weightDoc = new XmlDocument();
+            weightDoc.Load(@"weightData.xml");
+            XmlNodeList weightNodes = weightDoc.DocumentElement.SelectNodes("/workouts/Workout");
+            // string? readResult2;
+            // string menuSelection2 = "";
+            // Console.WriteLine("What would you like to focus on");
+            // Console.WriteLine("1. Full body");
+            // Console.WriteLine("2. Upper body");
+            // Console.WriteLine("3. Lower body");
+            // Console.WriteLine();
+            // Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
 
-            readResult2 = Console.ReadLine();
-            if (readResult2 != null)
+            // readResult2 = Console.ReadLine();
+            // if (readResult2 != null)
+            // {
+            //     menuSelection2 = readResult2.ToLower();
+            // }
+            foreach (XmlNode node in weightNodes)
             {
-                menuSelection2 = readResult2.ToLower();
+                if (node.Attributes["category"].Value == "weight")
+                {
+                    nodeList.Add(node);
+                }
+            }
+            Random rng2 = new Random();
+            int n2 = nodeList.Count;
+            while (n2 > 1)
+            {
+                n2--;
+                int k = rng2.Next(n2 + 1);
+                XmlNode value = nodeList[k];
+                nodeList[k] = nodeList[n2];
+                nodeList[n2] = value;
             }
 
+            foreach (XmlNode node in nodeList)
+            {
+                Console.WriteLine("Name: " + node["name"].InnerText + " Sets: " + node["sets"].InnerText + " Rest: " + node["rest"].InnerText);
+            }
+            nodeList = new List<XmlNode>();
+            Console.WriteLine("\n\rPress the Enter key to continue");
+            readResult = Console.ReadLine();
+
+            break;
+        case "3":
+            XmlDocument carDoc = new XmlDocument();
+            carDoc.Load(@"carData.xml");
+            XmlNodeList carNodes = carDoc.DocumentElement.SelectNodes("/workouts/Workout");
+
+            foreach (XmlNode node in carNodes)
+            {
+                if (node.Attributes["category"].Value == "cardio")
+                {
+                    nodeList.Add(node);
+                }
+            }
+            Random rng3 = new Random();
+            int n3 = nodeList.Count;
+            while (n3 > 1)
+            {
+                n3--;
+                int k = rng3.Next(n3 + 1);
+                XmlNode value = nodeList[k];
+                nodeList[k] = nodeList[n3];
+                nodeList[n3] = value;
+            }
+
+            foreach (XmlNode node in nodeList)
+            {
+                Console.WriteLine("Name: " + node["name"].InnerText + " Sets: " + node["sets"].InnerText + " Rest: " + node["rest"].InnerText);
+            }
+            nodeList = new List<XmlNode>();
             Console.WriteLine("\n\rPress the Enter key to continue");
             readResult = Console.ReadLine();
 
