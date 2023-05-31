35351f1b88826c91377266afc96c39a28138538f
stikie123 <stianbekker@gmail.com>
2023-05-31 17:36:30 +0200

    added cancel create feature

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Xml;
 using System.IO;
 using System.Xml.XPath;
+using System.Xml.Linq;
 
 string? readResult;
 string menuSelection = "";
@@ -19,11 +20,12 @@ do
 {
     Console.Clear();
     Console.WriteLine("Welcome to the My Prefect workout app. Your main menu options are:");
-    Console.WriteLine(" 1. Calisthenic Workout");
-    Console.WriteLine(" 2. Weight Workout");
-    Console.WriteLine(" 3. Cardio Workout");
-    Console.WriteLine(" 4. Custom workout");
-    Console.WriteLine(" 5. Add New workout");
+    Console.WriteLine(" 1. Calisthenic Workout(beta)");
+    Console.WriteLine(" 2. Weight Workout(beta)");
+    Console.WriteLine(" 3. Cardio Workout(beta)");
+    Console.WriteLine(" 4. Custom workout(alfa)");
+    Console.WriteLine(" 5. Add New workout(under construction)");
+    Console.WriteLine(" 6. Delete a workout(under construction)");
     Console.WriteLine();
     Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
 
@@ -76,9 +78,9 @@ do
             // string? readResult2;
             // string menuSelection2 = "";
             // Console.WriteLine("What would you like to focus on");
-            // Console.WriteLine("1. Full body");
-            // Console.WriteLine("2. Upper body");
-            // Console.WriteLine("3. Lower body");
+            // Console.WriteLine("1. Full body"); then arms abs legs all..
+            // Console.WriteLine("2. Upper body"); abs arms ecs...
+            // Console.WriteLine("3. Lower body"); legs
             // Console.WriteLine();
             // Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
 
@@ -246,9 +248,9 @@ do
             List<string> xmlFiles = new List<string> { @"calData.xml", @"weightData.xml", @"carData.xml" };
             foreach (string xmlFile in xmlFiles)
             {
-                XmlDocument doc = new XmlDocument();
-                doc.Load(xmlFile);
-                XmlNodeList nodes = doc.DocumentElement.SelectNodes("/workouts/Workout");
+                XmlDocument doc4 = new XmlDocument();
+                doc4.Load(xmlFile);
+                XmlNodeList nodes = doc4.DocumentElement.SelectNodes("/workouts/Workout");
 
                 foreach (XmlNode node in nodes)
                 {
@@ -268,6 +270,147 @@ do
             readResult = Console.ReadLine();
             break;
 
+        case "5":
+            string tags = "";
+            string focus = "";
+            string name = "";
+            string explanation = "";
+            string sets = "";
+            string rest = "";
+            string time = "";
+            string link = "";
+            string didCancel = "";
+            //checks if you want to cancel
+            do
+            {
+                Console.WriteLine("\n\r*Which type of workout would you like to add?");
+                Console.WriteLine(" 1. Calisthenics");
+                Console.WriteLine(" 2. Weight");
+                Console.WriteLine(" 3. Cardio");
+                string fileSelection = Console.ReadLine();
+                if (fileSelection == "cancel")
+                {
+                    didCancel = "true";
+                    break;
+                }
+                string selectedFile = "";
+                string category = "";
+                switch (fileSelection)
+                {
+                    case "1":
+                        selectedFile = @"calData.xml";
+                        category = "calisthenics";
+                        break;
+
+                    case "2":
+                        selectedFile = @"weightData.xml";
+                        category = "weight";
+                        break;
+                    case "3":
+                        selectedFile = @"carData.xml";
+                        category = "cardio";
+                        break;
+                    default:
+                        break;
+                }
+                //adddd cases
+                Console.WriteLine("*Enter the workout tags (e.g. beginner): ");
+                tags = Console.ReadLine();
+                if (tags == "cancel")
+                {
+                    didCancel = "true";
+                    break;
+                }
+                // adddd cases
+                Console.WriteLine("*Enter the workout focus (e.g. shoulders): ");
+                focus = Console.ReadLine();
+                if (focus == "cancel")
+                {
+                    didCancel = "true";
+                }
+
+                Console.WriteLine("*Enter the workout name (e.g. 3km Jog): ");
+                name = Console.ReadLine();
+                if (name == "cancel")
+                {
+                    didCancel = "true";
+                    break;
+                }
+
+                Console.WriteLine("*Enter the workout explanation: ");
+                explanation = Console.ReadLine();
+                if (explanation == "cancel")
+                {
+                    didCancel = "true";
+                    break;
+                }
+
+                Console.WriteLine("*Enter the number of sets and reps (e.g. 1x1): ");
+                sets = Console.ReadLine();
+                if (sets == "cancel")
+                {
+                    didCancel = "true";
+                    break;
+                }
+
+                Console.WriteLine("*Enter the rest time in seconds (e.g. 180): ");
+                rest = Console.ReadLine();
+                if (rest == "cancel")
+                {
+                    didCancel = "true";
+                    break;
+                }
+
+                Console.WriteLine("*Enter the workout time: ");
+                time = Console.ReadLine();
+                if (time == "cancel")
+                {
+                    didCancel = "true";
+                    break;
+                }
+
+                Console.WriteLine("Enter a link for the workout (e.g. https://www.example.com/workout): ");
+                link = Console.ReadLine();
+                if (link == "cancel")
+                {
+                    didCancel = "true";
+                    break;
+                }
+
+                // Check if any input is null or empty
+                if (string.IsNullOrEmpty(category) || string.IsNullOrEmpty(tags) || string.IsNullOrEmpty(focus) || string.IsNullOrEmpty(name) || string.IsNullOrEmpty(explanation) || string.IsNullOrEmpty(sets) || string.IsNullOrEmpty(rest) || string.IsNullOrEmpty(time))
+                {
+                    Console.WriteLine("Error: All fields are required. Please enter values for all fields.*");
+                }
+                else
+                {
+
+
+                    XDocument doc5 = XDocument.Load(selectedFile);
+
+                    // Create a new Workout element
+                    XElement workout = new XElement("Workout",
+                        new XAttribute("category", category),
+                        new XAttribute("tags", tags),
+                        new XAttribute("focus", focus),
+                        new XElement("name", name),
+                        new XElement("explanation", explanation),
+                        new XElement("sets", sets),
+                        new XElement("rest", rest, new XAttribute("value", rest)),
+                        new XElement("time", time),
+                        new XElement("link", link)
+                    );
+
+                    // Add the new Workout element to the workouts element
+                    doc5.Element("workouts").Add(workout);
+
+                    // Save the updated XML document
+                    doc5.Save(selectedFile);
+                }
+                Console.WriteLine("\n\rPress the Enter key to continue");
+                readResult = Console.ReadLine();
+            } while (didCancel != "true");
+            break;
         default:
             break;
     }
